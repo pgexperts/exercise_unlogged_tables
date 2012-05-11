972c27f0e1c7c28314acd165cb55ecf3f7f16a42
Jeff Frost <jeff@pgexperts.com>
2012-05-10 18:30:17 -0700

    add options for rows and total tables

--- exercise_unlogged_tables.py
+++ exercise_unlogged_tables.py
@@ -4,19 +4,27 @@
 import psycopg2
 import random
 import subprocess
+from optparse import OptionParser
+
+parser = OptionParser()
+
+parser.add_option('-t', "--total", action="store", type="int", dest="total", default=5000)
+parser.add_option('-r', "--rows", action="store", type="int", dest="rows", default=1000)
+
+(options, args) = parser.parse_args()
 
 conn = psycopg2.connect("dbname=unlogged_test user=jeff")
 cur = conn.cursor()
 conn.set_session(autocommit=True)
 cur.execute("SET synchronous_commit = 'off'")
 
-for tablenum in xrange(1000,5000):
+for tablenum in xrange(options.total):
     table = '"unlogged_tbl_' + str(tablenum) + '"'
     SQL = "CREATE UNLOGGED TABLE " + table + "(foo text);"
     print SQL
     cur.execute(SQL)
     #conn.commit()
-    noisewords = subprocess.Popen(["/home/jeff/git/noisewords/noisewords.py", "--rows=1000"], stdout=subprocess.PIPE)
+    noisewords = subprocess.Popen(["/home/jeff/git/noisewords/noisewords.py", "--rows=%d" % (options.rows) ], stdout=subprocess.PIPE)
     #copy_text = "COPY " + table + "FROM stdin"
     #cur.copy_expert(copy_text, noisewords.stdout)
     for noiseline in noisewords.stdout:
@@ -28,7 +36,7 @@ for tablenum in xrange(1000,5000):
     print vacuum_sql
     cur.execute(vacuum_sql)
 
-for tablenum in xrange(5000):
+for tablenum in xrange(options.total):
     table = '"unlogged_tbl_' + str(tablenum) + '"'
     SQL = "SELECT foo FROM" + table
     print SQL
